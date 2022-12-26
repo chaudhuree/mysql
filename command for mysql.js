1a9be967786507c0d2a00dc33447c1b1bcaf5bf2
chaudhuree <chaudhuree00@gmail.com>
2022-12-26 23:46:27 +0600

    docs: create table , rename table,  drop table, insert data

---
+++ command for mysql.js
@@ -0,0 +1,33 @@
+// create database
+
+// CREATE DATABASE college;
+
+// table with name student
+
+// CREATE TABLE student(
+// 	Roll int,
+//     Name varchar(15),
+//     Age int(4),
+//     Gender varchar(10),
+//     GPA double(3,2),
+//     City varchar(15),
+//     PRIMARY KEY(Roll)
+// );
+
+// rename table
+
+// RENAME TABLE student TO student_details ;
+
+// drop table
+
+// DROP TABLE student_details;
+
+// insert data in table
+
+// INSERT INTO student_details VALUES
+//   (56, 'sohan', 27, 'male', 4.88, 'panchbibi'),
+//   (12, 'mizan', 26, 'male', 5.00, 'joypurhat'),
+//   (43, 'hasina', 24, 'female', 3.27, 'dinajpur'),
+//   (48, 'kuddus', 26, 'male', 2.08, 'rangput'),
+//   (09, 'easha', 27, 'female', 5.00, 'narshingdi'),
+//   (22, 'rahim', 24, 'male', 3.58, 'panchbibi');
