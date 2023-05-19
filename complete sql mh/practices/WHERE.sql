305c82e2aec9074d2daa2bd772e9069f72c1dc32
chaudhuree <chaudhuree00@gmail.com>
2023-05-20 02:34:32 +0600

    where, exercise

--- complete sql mh/practices/WHERE.sql
+++ complete sql mh/practices/WHERE.sql
@@ -32,3 +32,14 @@ FROM
 WHERE
     birth_date <= '1990-01-01'
         AND points <= 1000;
+
+
+-- exercise
+
+-- For the order_items table, get the items
+-- for order #6
+-- where the total price is greater than 30
+USE store;
+SELECT *
+FROM order_items
+WHERE order_id = 6 AND unit_price * quantity > 30;
